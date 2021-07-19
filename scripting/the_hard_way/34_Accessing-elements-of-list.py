64f4872076097dac8732e020767aee2620d63f97
dan4ielo <dan4ielo@gmail.com>
2021-07-19 22:49:44 +0300

    LPTHW Update

---
+++ scripting/the_hard_way/34_Accessing-elements-of-list.py
@@ -0,0 +1,15 @@
+# Accessing elements of lists
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The first animal is at index 0 and is a bear
+# The animal with an index 0 is the first animal and is a bear
+
+# The third animal is at index 2 and is a peacock
+# The animal with an index 2 is the third animal and is a peacock
+
+# The fifth animal is at index 4 and is a whale
+# The animal with an index of 4 is the fifth animal and is a whale
+
+# The second animal is at index 1 and is a python
+# The animal with an index of 1 is the second animal and is a python
