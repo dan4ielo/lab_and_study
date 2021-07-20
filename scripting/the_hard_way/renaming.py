d8bc6ae7f80f82beae2705ced6d02400d41c53de
dan4ielo <dan4ielo@gmail.com>
2021-07-20 13:18:08 +0300

    renaming done - almost

--- scripting/the_hard_way/renaming.py
+++ scripting/the_hard_way/renaming.py
@@ -2,20 +2,29 @@
 
 import os
 
-def get_names(_file):
+def get_names(source):
     old_names = []
     new_names = []
     
+    lines = source.readlines()
+
+    for line in lines:
+        old_names.append(line.split(" . ")[0])
+        new_names.append(line.split(" . ")[1])
+
     return old_names, new_names # two lists containing the old names and the new ones
 
+source = input("Please specify a source file: ")
+
+with open(source, 'r') as names_list:
+    old_names, new_names = get_names(names_list)
 
 current_directory = os.getcwd()
 
 print (current_directory)
-print()
-
-name_changes = input("Please specify a source file: ")
-
-with open(name_changes, 'r') as names_list:
-    # get_names(names_list)
-    print (names_list.readlines())
+for old_name, new_name in zip(old_names, new_names):
+    # print(current_directory + "/" + old_name)
+    try:
+        os.rename(src = current_directory + "/" + old_name, dst = current_directory + "/" + new_name)
+    except FileNotFoundError:
+        continue
