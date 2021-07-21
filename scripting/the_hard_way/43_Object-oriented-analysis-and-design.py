1909f96dc86d4ee42c79c9daa8b14ff4f66e4a05
dan4ielo <dan4ielo@gmail.com>
2021-07-21 19:42:19 +0300

    excercise 43 complete

--- scripting/the_hard_way/43_Object-oriented-analysis-and-design.py
+++ scripting/the_hard_way/43_Object-oriented-analysis-and-design.py
@@ -124,3 +124,24 @@ class Map():
 a_map = Map('central_corridor')
 a_game = Engine(a_map)
 a_game.play()
+
+#===============================================================#
+
+# The process we just completed is known as a top-down approach.
+# It is called like that since it starts from the abstract idea
+# (the top) and drills down until you reach a solution. However,
+# there is an analogical approach that goes in the reverse order.
+
+# Bottom-up approach works with the following steps:
+
+# 1. Take a small piece of the problem; hack on some code and get
+# it to run barely.
+# 2. Refine the code into something more formal with classes and
+# automated tests.
+# 3. Extract the key concepts you're using and try to find 
+# research for them.
+# 4. Write up a description of what's really going on.
+# 5. Go back and refine the code, possibly throwing it out and 
+# starting over
+# 6. Repear, moving on to some other piece of the problem.
+
