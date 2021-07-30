157a7df90ea75478276d796392f8f00ac46d7811
dan4ielo <dan4ielo@gmail.com>
2021-07-30 14:13:11 +0300

    organisational commit - forgot to add something :/

---
+++ scripting/the_hard_way/Renaming/0.py
@@ -0,0 +1,37 @@
+# This file is used to rename all of the excercises from the book Learn Python the hard way
+
+import os
+
+def get_names(source):
+    old_names = []
+    new_names = []
+    
+    lines = source.readlines()
+
+    for line in lines:
+        try:
+            old_names.append(line.split(" . ")[0])
+            new_names.append(line.split(" . ")[1])
+        except IndexError:
+            continue
+    return old_names, new_names # two lists containing the old names and the new ones
+
+source = input("Please specify a source file: ")
+
+with open(source, 'r') as names_list:
+    old_names, new_names = get_names(names_list)
+
+current_directory = os.getcwd()
+
+print (current_directory)
+
+# Only option for now that does produce a good output
+
+# os.rename(src = current_directory + "/1.py", dst = current_directory + "/1_A-good-first-program.py")
+
+# All of the options below produce wierd symbols and mess up the naming
+
+# destination = current_directory + "/" + new_names[2]
+# os.rename(src = current_directory + "/" + old_names[2], dst = destination)
+# os.replace(src = current_directory + "/" + old_names[1], dst = current_directory + "/" + new_names[1])
+
